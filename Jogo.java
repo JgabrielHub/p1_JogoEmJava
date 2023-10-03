c6d1f3f0222952df0644987492c68b5543af3887
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-10-03 18:30:39 -0300

    p1(jogo): novo personagem

--- Jogo.java
+++ Jogo.java
@@ -1,33 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+
 public class Jogo {
   public static void main(String[] args) throws Exception {
     var gerador = new Random();
-    var personagem = new Personagem("John");
-    
-    
-    while(true){
-      int oQueFazer = gerador.nextInt(4); //[0..3[
-      
-      personagem.morrer();
-      switch(oQueFazer){
-        case 0:
-          personagem.cacar();
-          break;
-        case 1:
-        personagem.comer();
-          break;
-        case 2:
-        personagem.dormir();
-        break;
-        case 3:
-        personagem.morrer();
-        break;
+    List<Personagem> personagens = new ArrayList<>();
+    personagens.add(new Personagem("John", true));
+    personagens.add(new Personagem("Steve", true));
+
+    while (existeVivo(personagens)) {
+      for (Personagem personagem : personagens) {
+        if (personagem.vive()) {
+          int oQueFazer = gerador.nextInt(3); // [0..3[
+
+          switch (oQueFazer) {
+            case 0:
+              personagem.cacar();
+              break;
+            case 1:
+              personagem.comer();
+              break;
+            case 2:
+              personagem.dormir();
+              break;
+          }
+          personagem.exibirEstado();
+
+          personagem.morrer();
+
+          System.out.println("--------------------");
+          Thread.sleep(1000);
+        }
       }
-      personagem.exibirEstado();
-      
-      
-      System.out.println("********************");
-      Thread.sleep(1000);
     }
+  }
+
+  private static boolean existeVivo(List<Personagem> personagens) {
+    for (Personagem personagem : personagens) {
+      if (personagem.vive()) {
+        return true;
+      }
+    }
+    System.out.println("Game Over");
+    return false;
   }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,90 +1,92 @@
 public class Personagem {
- 
-  //atributos ou propriedades
-  private String nome;
-  private int energia = 10;
-  private int fome = 10;
-  private int sono = 0;
-  private int item = 0;
 
-  //construtor: lista de parâmetros vazia
+  // atributos ou propriedades
+  private String nome;
+  private int energia = 5;
+  private int fome = 0;
+  private int sono = 0;
+  private int item = 4;
+  private boolean vivo = true;
+  // construtor: lista de parâmetros vazia
   // Personagem(){
 
   // }
 
-  Personagem(String nome){
+  Personagem(String nome, boolean vivo) {
     this.nome = nome;
+    this.vivo = vivo;
   }
 
-  Personagem(String nome, int energia, int fome, int sono, int item){
+  Personagem(String nome, int energia, int fome, int sono, int item, boolean vivo) {
     this.nome = nome;
+    this.vivo = vivo;
     if (energia >= 0 && energia <= 10)
       this.energia = energia;
     if (fome >= 0 && fome <= 10)
       this.fome = fome;
-    if(sono >= 0 && sono <= 10)
+    if (sono >= 0 && sono <= 10)
       this.sono = sono;
-      if(item >= 0 && item <= 10 )
-      this.item = item;  
+    if (item >= 0 && item <= 10)
+      this.item = item;
+
   }
 
-  //comportamentos (métodos)
-  void cacar(){
-    if(energia >= 2){
+  // comportamentos (métodos)
+  void cacar() {
+    if (energia >= 2) {
       System.out.println(nome + " caçando...");
       energia -= 2; // energia = energia - 2;
       item = Math.min(10, item + 1);
-    }
-    else{
-      System.out.println (nome + " sem energia para caçar...");
+    } else {
+      System.out.println(nome + " sem energia para caçar...");
     }
     fome = Math.min(fome + 1, 10);
-    sono = Math.min(sono + 1 ,10);
+    sono = Math.min(sono + 1, 10);
   }
 
-  void comer(){
-    if (fome >= 1 && item >= 1){
+  void comer() {
+    if (fome >= 1 && item >= 1) {
       System.out.println(nome + " comendo...");
       // operador ternário
       energia = energia + 1 > 10 ? 10 : energia + 1;
       fome--;
       item--;
-    } else if(item >= 1){
+    } else if (item >= 1) {
       System.out.println(nome + " sem fome...");
-    } else{
+    } else {
       System.out.println(nome + " sem alimento...");
     }
   }
 
-  void dormir(){
-    if(sono > 0){
+  void dormir() {
+    if (sono > 0) {
       System.out.println(nome + " dormindo...");
       sono -= 1;
       energia = Math.min(10, energia + 1);
-    }
-    else{
+    } else {
       System.out.println(nome + " sem sono");
     }
 
   }
 
-  void exibirEstado(){
+  void exibirEstado() {
     System.out.printf(
-      "%s: e: %d, f: %d, s: %d, i: %d\n",
-      nome,
-      energia,
-      fome,
-      sono,
-      item
-    );
+        "%s: e: %d, f: %d, s: %d, i: %d\n",
+        nome,
+        energia,
+        fome,
+        sono,
+        item);
   }
 
-  void morrer(){
-      if (energia <= 0){
-        System.out.println(nome + " morreu");
-        System.exit(0);
-      }
+  void morrer() {
+    if (energia <= 0) {
+      vivo = false;
+      System.out.println(nome + " morreu");
+    }
   }
-  
- 
+
+  public boolean vive() {
+    return vivo;
+  }
 }
